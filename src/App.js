c53057014ec878afb99d6d9d07822336048f7176
Nontando <carolnontando@gmail.com>
2024-03-23 20:29:16 +0200

    edited footer

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York" />
         <footer>
-          This project was coded by
+          This project was coded by {""}
           <a href="shecode.io" target="_blank">
             Ntando Ngcobo
           </a>
-          and is
+          {""} and is open-sourced on {""}
           <a href="shecodes.io" target="_blank">
-            open-sourced on GitHub
+            GitHub
           </a>
+          {""} and Hosted on{""} <a href="/">Netlify</a>
         </footer>
       </div>
     </div>
